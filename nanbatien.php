54f0330be7d0c9ebfc56dc71c374efcb89243b13
iamgyz <my.good.life.2013@gmail.com>
2013-06-23 22:13:53 +0800

    Current Version
    南霸天學分速算系統_ver2

---
+++ nanbatien.php
@@ -0,0 +1,505 @@
+<!--
+GYZ 2013/4/14
+版權所有 翻印必究
+Special Thanks to Kuri !!
+-->
+<html>
+<head>
+<meta http-equiv="Content-Language" content="zh-tw">
+<title>新。南霸天學分速算系統</title>
+<meta http-equiv="Content-Type" content="text/html; charset=big5" />
+</head>
+<body>
+<style type="text/css" >
+   body {
+      font-family: Arial;
+      font-size: 12pt;
+      margin: 0px;
+      padding: 0px;
+      background-color: #eee;
+    }
+    table {
+    border-collapse: collapse;
+    border-left: 1px solid #1F2A34;
+    border-right: 1px solid #1F2A34;
+    border-spacing: 1px;
+    font-family: Tahoma,Verdana,Arial;
+    width: 900px;
+}
+thead th {
+    background: none repeat scroll 0 0  #4682B4;
+	/*  1:#4682B4 steeblue    2:#6B8E23  OliveDrab    3.#9370DB  medium purple*/
+    border-color: black #1F2A34;
+    border-style: solid;
+    border-width: 1px 1px;
+    color: #FFFFFF;
+    font-size: 14px;
+    height: 30px;
+    text-align: center;
+}
+tbody td {
+    background: none repeat scroll 0 0 #FFFFFF;
+    border: 1px solid #CCCCFF;
+    color: #000000;
+    font-size: 12px;
+    height: 15px;
+    padding-left: 1px;
+}
+tbody tr .odd {
+    background: none repeat scroll 0 0 #AEE239;
+}
+tbody td a img {
+    border: 0 none;
+    margin: 0;
+    padding: 0;
+}
+tbody td a:link {
+    color: #FF6600;
+    font-size: 12px;
+    font-weight: bold;
+    text-decoration: underline;
+}
+tbody td a:hover {
+    color: #FF6600;
+    font-size: 12px;
+    font-weight: bold;
+    text-decoration: underline;
+}
+tbody td a:visited {
+    color: #FF6600;
+    font-size: 12px;
+    font-weight: bold;
+    text-decoration: underline;
+}
+tbody th {
+    background: none repeat scroll 0 0 #373B3E;
+    border: 1px solid #2D3134;
+    color: #080808;
+    padding-left: 4px;
+    text-align: left;
+}
+tbody th a:link {
+    color: #FFFFFF;
+    font-size: 12px;
+    font-weight: bolder;
+    text-decoration: underline;
+}
+tbody th a:hover {
+    color: #FFFFFF;
+    font-size: 13px;
+    font-weight: bolder;
+}
+tbody th a:visited {
+    color: #FFFFFF;
+    font-size: 12px;
+    font-weight: bolder;
+    text-decoration: line-through;
+}
+tbody tr:hover {
+    background: none repeat scroll 0 0 #333333;
+}
+tbody a[title^="Download"] {
+    color: #FF6600;
+    font-size: 12px;
+    text-align: center;
+}
+tbody a[title^="Download"]:hover {
+    color: #FF6600;
+    font-size: 12px;
+    text-decoration: underline;
+}
+tbody a[title^="Download"]:visited {
+    color: #FF6600;
+    font-size: 12px;
+    text-decoration: line-through;
+}
+tfoot {
+    background: none repeat scroll 0 0 #1F2A34;
+    border-left: 1px solid #1F2A34;
+    border-right: 1px solid #1F2A34;
+    color: #FFFFFF;
+    font-size: 18px;
+    text-align: center;
+}
+tfoot td {
+    font-style: italic;
+}
+table caption {
+    background: none repeat scroll 0 0 #1F2A34;
+    border-left: 1px solid #1F2A34;
+    border-right: 1px solid #1F2A34;
+    color: #FFFFFF;
+    font-size: 24px;
+    padding-bottom: 10px;
+    padding-top: 10px;
+    text-align: center;
+}
+</style>
+<p align="center"><font face="微軟正黑體"><b><span style="font-size: 22pt; ">新。南霸天學分速算系統
+<p align="center"><u><font color="#FF0000">結果僅供參考!!!!!&nbsp;&nbsp; 結果僅供參考!!!!&nbsp;&nbsp; 結果僅供參考!!!</font></u></p>	
+<table align="center">
+    
+  <th>  	<font color="#FFFFFF">  	<b>狀態:</b></font>   </th>
+       <tr><td> <div id="name">資料查詢中，請稍候!<br></div></td></tr>
+
+</table>
+
+<table align="center">
+<th><font color="#FFFFFF"><b>學分計算結果:</b></font></th>
+     <tr><td>   <div id="now_has"></div></td></tr>
+
+</table>      
+	
+	
+<table align="center">
+		<th>	<p><font color="#FFFFFF"><b>核心通識:</b></font> </th>
+			<div id='show'></div>
+			<tr><td>
+			<div id='english'>
+				<p><font color="#FF0000"><b><u>國際語言</font><br></u></b>
+			</div>
+			
+			<b><font color="#800080"><span id='english_s'>0</span><a>學分</a></b></font> </td></tr>
+				<tr><td><div id='chinese'>
+		
+			<font color="#FF0000"><b><u>基礎國文</font><br></u></b>
+				</div><b><font color="#800080"><span id='chinese_s'>0</span><a>學分</a></b></font></td></tr>
+
+			<tr><td><div id='art'>
+			
+			<font color="#FF0000"><b><u>哲學與藝術</font><br></u></b>
+			</div><b>
+			<font color="#800080"><span id='art_s'>0</span><a>學分</a></b></font></td></tr>
+			<tr><td><div id='civic'>
+			
+			<font color="#FF0000"><b><u>公民與歷史</font><br></u></b>
+
+			</div><b><font color="#800080"><span id='civic_s'>0</span><a>學分</a></b></font></td></tr>
+</table>
+<table align="center">
+      <th> <p><font color="#FFFFFF"><b>一般通識:</b></font> </th>
+	<tr><td>	<div id='science'>
+		<p><font color="#FF0000"><b><u>自然與工程科學</font><br></u></b>
+			</div><b><font color="#800080"><span id='science_s'>0</span><a>學分</a></b></font></td></tr>
+
+		<tr><td>	<div id='life'>
+		<p><font color="#FF0000"><b><u>生命科學與健康</font><br></u></b>
+			</div><b><font color="#800080"><span id='life_s'>0</span><a>學分</a></b></font></td></tr>
+
+   	<tr><td> <div id='social'>
+    <font color="#FF0000"><b><u>社會科學</font><br></u></b>
+			</div><b><font color="#800080"><span id='social_s'>0</span><a>學分</a></b></font></td></tr>
+
+    
+    	<tr><td><div id='human'>
+    <font color="#FF0000"><b><u>人文學</font><br></u></b>
+			</div><b><font color="#800080"><span id='human_s'>0</span><a>學分</a></b></font></td></tr>
+    
+    	<tr><td><div id='mix'>
+    <font color="#FF0000"><b><u>融合通識</font><br></u></b>
+			</div><b><font color="#800080"><span id='mix_s'>0</span><a>學分</a></b></font>   </td></tr>
+			 
+			 <tr><td><div id='all_in'>
+	<font color="#FF0000"><b><u>科際整合</font><br></u></b></div>
+	  <b><font color="#800080"><span id='all_in_s'>0</span><a>學分</a></b></font></td></div></td></tr>
+
+</table>
+<table align="center">
+
+<th><p><font color="#FFFFFF">一般<b>課程:</b></font></th>
+      <tr><td>  <div id='require'>
+<p><font color="#FF0000"><b><u>本系必修</font><br></u></b>
+			</div><b><font color="#800080"><span id='require_s'>0</span><a>學分</a></b></font></td></tr>
+
+	    
+		<tr><td><div id='choose'>
+<font color="#FF0000"><b><u>本系選修</font><br></u></b>
+			</div><b><font color="#800080"><span id='choose_s'>0</span><a>學分</a></b></font>  </td></tr>  </div>  
+	
+
+<tr><td><div id='out_choose'>
+<font color="#FF0000"><b><u>外系選修</font><br></u></b>
+			</div><b><font color="#800080"><span id='out_choose_s'>0</span><a>學分</a></b></font>  </td></tr>  </div>  
+
+<tr><td><div id='serve'>
+<font color="#FF0000"><b><u>服務學習</font><br></u></b>
+			</div><b><font color="#800080"><span id='serve_s'>0</span><a>學分</a></b></font>  </td></tr>  </div>  
+			
+<tr><td><div id='pe'>
+<font color="#FF0000"><b><u>體育</font><br></u></b>
+			</div><b><font color="#800080"><span id='pe_s'>0</span><a>學分</a></b></font>  </td></tr>  </div>  
+			
+						
+
+</table>	
+
+
+
+
+           	<p align="center"><font face="微軟正黑體"><b><span style="font-size: 12pt; ">2013 GYZ ALL RIGHTS RESERVED
+			<a href="mailto:my.good.life.2013@gmail.com">
+			my.good.life.2013@gmail.com</a></span></b></font></p>
+<script>
+//序號 :0|系所 :1|課程碼 :2|科目名稱 :3|課程別 :4|學分 :5|必/選修 :6|分數 :7|承認Y/N :8|通識領域:9|
+var serve_course=0//計算服務學習
+var pe_course=0//計算PE
+var dept="";
+function readme(x)
+{
+	if(x==1){document.getElementById('readme').style.display='block';}
+	if(x==2){document.getElementById('readme').style.display='none';}
+}
+
+function get_info(data)
+{
+	//5=>0099上   6=>系 18=>學號 19=>名子
+	var str=data.split(" ");
+	document.getElementById('name').innerHTML+= str[5]+" 課表已加入!<br>";
+	//用學號前兩碼判斷
+	dept=str[18].substring(0,2);
+}
+
+function process(data)
+{
+	var str=data.split("\n");
+	if(str.length>0)
+	{
+	for(i=0;i<str.length;i++)
+	{
+		if(str[i].indexOf('男')!=-1||str[i].indexOf('女')!=-1)
+		{
+				if(str[i].indexOf('修')==-1)
+				{
+					get_info(str[i]);
+					var show=str[i];
+					//alert(show+"課表已加入完成!");
+					}
+					else
+					{
+					add_result(str[i]) //不然婦"女"使  羽球"男女" 會被剔除
+				}//else
+		}//if
+		else
+		{
+		add_result(str[i])
+		}
+	}//for
+}
+
+calculate_score();//calculate score
+
+
+}
+
+function calculate_score()
+{
+	document.getElementById('now_has').innerHTML="";
+	//計算服務學習
+	if(serve_course>=3){document.getElementById('now_has').innerHTML+="服務學習: 已修畢<br>";}
+	else {document.getElementById('now_has').innerHTML+="服務學習: 尚未修畢<br>";}
+	//計算pe
+	if(pe_course>=4){document.getElementById('now_has').innerHTML+="體育課程: 已修畢<br>";}
+	else {document.getElementById('now_has').innerHTML+="體育課程: 尚未修畢<br>";}
+
+	//計算核心通識
+	element1=document.getElementById('english_s');
+	tmp1=parseInt(element1.innerHTML);
+	element1=document.getElementById('chinese_s');
+	tmp2=parseInt(element1.innerHTML);
+	element1=document.getElementById('art_s');
+	tmp3=parseInt(element1.innerHTML);
+	element1=document.getElementById('civic_s');
+	tmp4=parseInt(element1.innerHTML);
+	if(tmp1>=4 & tmp2>=4 && tmp3>=4 && tmp4>=4)
+	{document.getElementById('now_has').innerHTML+="核心通識: 已修畢<br>";}
+	else  {document.getElementById('now_has').innerHTML+="核心通識: 尚未修畢<br>";}
+	//計算一般通識
+	var science=0,social=0,human=0,life=0,all_in=0,mix=0;
+	element1=document.getElementById('science_s');
+	science=parseInt(element1.innerHTML);
+	element1=document.getElementById('social_s');
+	social=parseInt(element1.innerHTML);
+	element1=document.getElementById('human_s');
+	human=parseInt(element1.innerHTML);
+	element1=document.getElementById('life_s');
+	life=parseInt(element1.innerHTML);
+	element1=document.getElementById('all_in_s');
+	all=parseInt(element1.innerHTML);
+
+	element1=document.getElementById('mix');
+	mix=parseInt(element1.innerHTML);
+
+	if(science>=2&&social>=2&&human>=2&&life>=2&&mix>=2&&science+social+human+life+mix+all_in>=16)
+	{
+		document.getElementById('now_has').innerHTML+="一般通識: 已修畢<br>";
+	}
+	else 
+	{
+		document.getElementById('now_has').innerHTML+="一般通識: 尚未修畢<br>";
+	}
+	//document.getElementById('now_has').innerHTML+="一般通識: 計算公式尚未建立<br>";
+
+
+}
+
+
+//序號 :0|系所 :1|課程碼 :2|科目名稱 :3|課程別 :4|學分 :5|必/選修 :6|分數 :7|承認Y/N :8|通識領域:9|
+function add_result(data)
+{
+	//此版本要用空白切!
+	var str=data.split(" ");
+
+	if(str.length>0&&data.indexOf('序號')==-1&&str[8]!=null&&str[7].indexOf('退選')==-1)//其實大於一就好 
+	{
+		if(parseInt(str[7])>=60)
+		{
+			if(str[3]!=null&&str[3].indexOf('英文')!=-1){document.getElementById('english').innerHTML+=str[3]+"("+str[5]+"學分)<br>";element=document.getElementById('english_s');tmp=parseInt(element.innerHTML);element.innerHTML=tmp+parseInt(str[5]);}
+			else if(str[3]!=null&&str[3].indexOf('基礎國文')!=-1){document.getElementById('chinese').innerHTML+=str[3]+"("+str[5]+"學分)<br>";element=document.getElementById('chinese_s');tmp=parseInt(element.innerHTML);element.innerHTML=tmp+parseInt(str[5]);}
+			else if(str[9]!=null&&str[9].indexOf('哲學與藝術')!=-1){document.getElementById('art').innerHTML+=str[3]+"("+str[5]+"學分)<br>";element=document.getElementById('art_s');tmp=parseInt(element.innerHTML);element.innerHTML=tmp+parseInt(str[5]);}
+			else if(str[9]!=null&&str[9].indexOf('公民與歷史')!=-1){document.getElementById('civic').innerHTML+=str[3]+"("+str[5]+"學分)<br>";element=document.getElementById('civic_s');tmp=parseInt(element.innerHTML);element.innerHTML=tmp+parseInt(str[5]);}
+			else if(str[1]!=null&&str[1].indexOf('AG')!=-1){document.getElementById('civic').innerHTML+=str[3]+"("+str[5]+"學分)<br>";element=document.getElementById('civic_s');tmp=parseInt(element.innerHTML);element.innerHTML=tmp+parseInt(str[5]);}
+			else if(str[9]!=null&&str[9].indexOf('自然與工程科學')!=-1){document.getElementById('science').innerHTML+=str[3]+"("+str[5]+"學分)<br>";element=document.getElementById('science_s');tmp=parseInt(element.innerHTML);element.innerHTML=tmp+parseInt(str[5]);}
+
+			else if(str[9]!=null&&str[9].indexOf('生命科學')!=-1){document.getElementById('life').innerHTML+=str[3]+"("+str[5]+"學分)<br>";element=document.getElementById('life_s');tmp=parseInt(element.innerHTML);element.innerHTML=tmp+parseInt(str[5]);}
+
+			else if(str[9]!=null&&str[9].indexOf('社會科學')!=-1){document.getElementById('social').innerHTML+=str[3]+"("+str[5]+"學分)<br>";element=document.getElementById('social_s');tmp=parseInt(element.innerHTML);element.innerHTML=tmp+parseInt(str[5]);}
+			else if(str[9]!=null&&str[9].indexOf('人文學')!=-1){document.getElementById('human').innerHTML+=str[3]+"("+str[5]+"學分)<br>";element=document.getElementById('human_s');tmp=parseInt(element.innerHTML);element.innerHTML=tmp+parseInt(str[5]);}
+			else if(str[9]!=null&&str[9].indexOf('融合通識')!=-1){document.getElementById('mix').innerHTML+=str[3]+"("+str[5]+"學分)<br>";element=document.getElementById('mix_s');tmp=parseInt(element.innerHTML);element.innerHTML=tmp+parseInt(str[5]);}
+			else if(str[9]!=null&&str[9].indexOf('科際')!=-1){document.getElementById('all_in').innerHTML+=str[3]+"("+str[5]+"學分)<br>";element=document.getElementById('all_in_s');tmp=parseInt(element.innerHTML);element.innerHTML=tmp+parseInt(str[5]);}
+			else if(str[9]!=null&&str[3].indexOf('服務學習')!=-1){ serve_course++;document.getElementById('serve').innerHTML+=str[3]+"("+str[5]+"學分)<br>";element=document.getElementById('serve_s');tmp=parseInt(element.innerHTML);element.innerHTML=tmp+parseInt(str[5]);}
+
+			else if(str[9]!=null&&str[3].indexOf('體育（一）')!=-1){pe_course++; document.getElementById('pe').innerHTML+=str[3]+"("+str[5]+"學分)<br>";element=document.getElementById('pe_s');tmp=parseInt(element.innerHTML);element.innerHTML=tmp+parseInt(str[5]);}
+			else if(str[9]!=null&&str[1].indexOf('A2')!=-1){ pe_course++;document.getElementById('pe').innerHTML+=str[3]+"("+str[5]+"學分)<br>";element=document.getElementById('pe_s');tmp=parseInt(element.innerHTML);element.innerHTML=tmp+parseInt(str[5]);}
+
+
+			else if(str[6].indexOf('必')!=-1)
+			{
+				if(str[1].indexOf(dept)==-1&&str[1].indexOf('A2')==-1)//體育課算在必修
+				{document.getElementById('out_choose').innerHTML+=str[3]+"("+str[5]+"學分)<br>";element=document.getElementById('out_choose_s');tmp=parseInt(element.innerHTML);element.innerHTML=tmp+parseInt(str[5]);}
+				else
+				{
+				document.getElementById('require').innerHTML+=str[3]+"("+str[5]+"學分)<br>";element=document.getElementById('require_s');tmp=parseInt(element.innerHTML);element.innerHTML=tmp+parseInt(str[5]);
+				}
+
+			}
+			else if(str[6].indexOf('選')!=-1)
+			{
+				if(str[1].indexOf(dept)==-1&&str[1].indexOf('A2')==-1)//體育課算在必修
+				{document.getElementById('out_choose').innerHTML+=str[3]+"("+str[5]+"學分)<br>";element=document.getElementById('out_choose_s');tmp=parseInt(element.innerHTML);element.innerHTML=tmp+parseInt(str[5]);}
+				else
+				{
+				document.getElementById('choose').innerHTML+=str[3]+"("+str[5]+"學分)<br>";element=document.getElementById('choose_s');tmp=parseInt(element.innerHTML);element.innerHTML=tmp+parseInt(str[5]);
+				}//else
+			}
+		}//if >60
+	}//if
+
+}
+</script>
+
+
+<?php
+//如果直接get content會顯示請開啟COOKIE
+//所以要先接  再當參數send
+function getData($year,$sem)
+{
+	$ID=$_POST['id'];
+	$PWD=$_POST['pwd'];
+	$httphandle = fopen("http://140.116.165.72:8888/ncku/qrys05.asp","r");
+	$meta = stream_get_meta_data($httphandle);
+	for ($j = 0; isset($meta['wrapper_data'][$j]); $j++) {
+	   $httpline = $meta['wrapper_data'][$j];
+	   @list($header,$parameters) = explode(";",$httpline,2);
+	   @list($attr,$value) = explode(":",$header,2);
+	   if (strtolower(trim($attr)) == "set-cookie") {
+		  $cookie = trim($value);
+		  break;
+	   }
+	}
+	fclose($httphandle);
+
+	$opts = array('http' => array('header'=> 'Cookie: ' . $cookie."\r\n"));
+	$context = stream_context_create($opts);
+	if($year==101&&$sem==2)
+	$page=file_get_contents('http://140.116.165.72:8888/ncku/qrys05.asp?ID='.$ID.'&PWD='.$PWD.'&submit1=0101下',false, $context);
+	if($year==101&&$sem==1)
+	$page=file_get_contents('http://140.116.165.72:8888/ncku/qrys05.asp?ID='.$ID.'&PWD='.$PWD.'&submit1=0101上',false, $context);
+	if($year==100&&$sem==2)
+	$page=file_get_contents('http://140.116.165.72:8888/ncku/qrys05.asp?ID='.$ID.'&PWD='.$PWD.'&submit1=0100下',false, $context);
+	if($year==100&&$sem==1)
+	$page=file_get_contents('http://140.116.165.72:8888/ncku/qrys05.asp?ID='.$ID.'&PWD='.$PWD.'&submit1=0100上',false, $context);
+	if($year==99&&$sem==2)
+	$page=file_get_contents('http://140.116.165.72:8888/ncku/qrys05.asp?ID='.$ID.'&PWD='.$PWD.'&submit1=0099下',false, $context);
+	if($year==99&&$sem==1)
+	$page=file_get_contents('http://140.116.165.72:8888/ncku/qrys05.asp?ID='.$ID.'&PWD='.$PWD.'&submit1=0099上',false, $context);
+	if($year==98&&$sem==2)
+	$page=file_get_contents('http://140.116.165.72:8888/ncku/qrys05.asp?ID='.$ID.'&PWD='.$PWD.'&submit1=0098下',false, $context);
+	if($year==98&&$sem==1)
+	$page=file_get_contents('http://140.116.165.72:8888/ncku/qrys05.asp?ID='.$ID.'&PWD='.$PWD.'&submit1=0098上',false, $context);
+	if($year==97&&$sem==2)
+	$page=file_get_contents('http://140.116.165.72:8888/ncku/qrys05.asp?ID='.$ID.'&PWD='.$PWD.'&submit1=0097下',false, $context);
+	if($year==97&&$sem==1)
+	$page=file_get_contents('http://140.116.165.72:8888/ncku/qrys05.asp?ID='.$ID.'&PWD='.$PWD.'&submit1=0097上',false, $context);
+	if($year==96&&$sem==2)
+	$page=file_get_contents('http://140.116.165.72:8888/ncku/qrys05.asp?ID='.$ID.'&PWD='.$PWD.'&submit1=0096下',false, $context);
+	if($year==96&&$sem==1)
+	$page=file_get_contents('http://140.116.165.72:8888/ncku/qrys05.asp?ID='.$ID.'&PWD='.$PWD.'&submit1=0096上',false, $context);
+
+	$page=strip_tags($page);
+	$data=split("\n",$page);
+	for($i=0;$i<sizeof($data);$i++)
+	{
+		$data[$i]=preg_replace('/\s/'," ", $data[$i]);
+	}
+	//加入學號資訊
+	if(sizeof($data)>=56)
+	{
+		echo "<script>process(\"".$data[56]."\")</script>";
+	}
+
+	for($i=74;$i<sizeof($data);$i++)
+	{
+		if(($i+17)>sizeof($data))break;//要加這行!不然有可能最後一個offset會爆
+		$tmp=preg_replace('/ /',"",$data[$i])." ".preg_replace('/ /',"",$data[$i+3])." ".preg_replace('/ /',"",$data[$i+5])." ".preg_replace('/ /',"",$data[$i+7])." ".preg_replace('/ /',"",$data[$i+9])." ".preg_replace('/ /',"",$data[$i+10])." ".preg_replace('/ /',"",$data[$i+11])." ".preg_replace('/ /',"",$data[$i+13])." ".preg_replace('/ /',"",$data[$i+15])." ".preg_replace('/ /',"",$data[$i+17]);
+
+		echo "<script>process(\"".$tmp."\")</script>";
+		//Magic!如果後方有紅字加21 沒有的話 加22
+		if(strlen(preg_replace('/ /',"",$data[$i+17]))==0)$i=$i+22;
+		else $i=$i+21;
+	}//for
+
+}//function
+
+
+//main fuction
+	//check id and password
+	if(strpos(" ".$_POST['id'],'\'') || strpos(" ".$_POST['id'],'\"')||strpos( " ".$_POST['pwd'],'\'')||strpos(" ".$_POST['pwd'],'\"'))
+	{
+	echo "<script>alert('查無資料，請重新查明後再播，謝謝!')</script>";
+	exit;
+	}
+	else if(strlen($_POST['id'])<=1 || strlen($_POST['pwd'])<=1)
+	{
+	echo "<script>alert('查無資料，請重新查明後再播，謝謝!')</script>";
+	exit;
+	}
+	else
+	{
+	//call function
+		getData(97,1);
+		getData(97,2);
+		getData(98,1);
+		getData(98,2);
+		getData(99,1);
+		getData(99,2);
+		getData(100,1);
+		getData(100,2);
+		getData(101,1);
+	//getData(101,2);
+	}	
+	echo "<script>document.getElementById('name').innerHTML+=\" 學分計算完成!!!!<br>\";</script>"	
+?>
+
+</body>
+</html>
+
